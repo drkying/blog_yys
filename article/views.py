673c877682670890a01aa619c7235bfedc7615bb
yatsun <yatsun@example.com>
2021-05-18 20:56:10 +0800

    引进了Markdown语法以及代码高亮扩展

--- article/views.py
+++ article/views.py
@@ -16,9 +16,16 @@ def article_list(request):
 
 # 文章详情
 def article_detail(request, id):
-    # 取出相应的文章
     article = ArticlePost.objects.get(id=id)
-    # 需要传递给模板的对象
+
+    # 将markdown语法渲染成html样式
+    article.body = markdown.markdown(article.body,
+        extensions=[
+        # 包含 缩写、表格等常用扩展
+        'markdown.extensions.extra',
+        # 语法高亮扩展
+        'markdown.extensions.codehilite',
+        ])
+
     context = { 'article': article }
-    # 载入模板，并返回context对象
     return render(request, 'article/detail.html', context)
